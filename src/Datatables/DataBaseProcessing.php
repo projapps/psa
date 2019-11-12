2bedf60950b49d2442aea3c897a9c60861343f36
James Ng <james.ng@projapps.com>
2019-11-12 18:38:03 +0800

    Implement add() for generic text data to table

--- src/Datatables/DataBaseProcessing.php
+++ src/Datatables/DataBaseProcessing.php
@@ -9,7 +9,15 @@ class DataBaseProcessing
     static function add ( $data, $db, $table, $columns )
     {
         $pluck = ServerSideProcessing::pluck($columns, 'db');
-        $sql = "INSERT INTO " . $table . "(" . implode(", ", $pluck) . ") VALUES()";
-        return $sql;
+        $sql = "INSERT INTO " . $table . "(" . implode(", ", $pluck) .
+            ") VALUES(:" . implode(", :", $pluck) . ")";
+        $stmt = $db->prepare($sql);
+        foreach ($columns as $column) {
+            $key = $column['db'];
+            $stmt->bindValue(':' . $key, $data->$key);
+        }
+        $stmt->execute();
+        return $db->lastInsertId();
     }
+    
 }
