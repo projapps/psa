91fe347994e500d2ede5a6f5f55df09522b96ee5
James Hian Ng <james.ng@projapps.com>
2019-12-30 16:20:12 +0800

    Fix add/edit table template page with validation

--- src/Controllers/SchemaController.php
+++ src/Controllers/SchemaController.php
@@ -13,4 +13,17 @@ class SchemaController
     public function __construct(ContainerInterface $container) {
         $this->container = $container;
     }
+
+    public function add(Request $request, Response $response, $args) {
+        if ($this->isAuthorised($request)) {
+            $data = $request->getParsedBody();
+            var_dump($data);
+        }
+    }
+
+    private function isAuthorised(Request $request) {
+        $session = $request->getAttribute('session');
+        $username = isset($session['username']) ? $session['username'] : '';
+        return ($username == 'admin');
+    }
 }
